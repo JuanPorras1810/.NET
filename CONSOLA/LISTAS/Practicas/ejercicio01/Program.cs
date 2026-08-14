f3fe4255769e74e38d75e8c22fc33a19df05c2b7
JUAN PORRAS <juanporras1810@gmail.com>
2026-08-14 17:10:23 -0500

    Ejercicios de listas

---
+++ CONSOLA/LISTAS/Practicas/ejercicio01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Aprendiz> aprendices = new List<Aprendiz>();
+            aprendices.Add(new Aprendiz { Id = 1, Nombre = "Juan Porras", Direccion = "Tv 18 #43-35", Telefono = "323344232", Estado = true });
+            aprendices.Add(new Aprendiz { Id = 2, Nombre = "María López", Direccion = "Cl 52 #20-18", Telefono = "310456789", Estado = true });
+            aprendices.Add(new Aprendiz { Id = 3, Nombre = "Carlos Gómez", Direccion = "Cra 25 #15-42", Telefono = "315678234", Estado = true });
+
+            foreach (var item in aprendices)
+            {
+                Console.WriteLine($"Nombre: {item.Nombre}");
+            }
+        }
+    }
+}
