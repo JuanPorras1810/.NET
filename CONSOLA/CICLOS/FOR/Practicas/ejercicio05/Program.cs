d6db0c1548aee66b2933cc03dfb1dff889e511ab
JUAN PORRAS <juanporras1810@gmail.com>
2026-08-13 22:20:11 -0500

    Ejercicios de ciclos

---
+++ CONSOLA/CICLOS/FOR/Practicas/ejercicio05/Program.cs
@@ -0,0 +1,19 @@
+﻿//Números pares en rango: Muestre todos los numeros pares entre 1 y 50.
+
+Console.WriteLine("=== NUMEROS PARES 1 - 50 ===");
+
+//Ciclo
+for (int i = 1; i <= 50; i++)
+{
+	if (i % 2 == 0)
+	{
+		if (i == 50)
+		{
+			Console.Write(i);
+		}
+		else 
+		{ 
+			Console.Write($"{i}, ");
+		}
+	}
+}
---
+++ CONSOLA/CICLOS/FOR/Practicas/ejercicio06/Program.cs
@@ -0,0 +1,17 @@
+﻿//Triangulo de asteriscos: Dibuje un triángulo rectángulo de '*' según la altura ingresada.
+
+Console.WriteLine("=== GENERADOR DE TRIANGULOS ===");
+
+//Captura de datos
+Console.Write("Ingrese la altura: ");
+int altura = int.Parse(Console.ReadLine());
+
+//Ciclo
+for (int i = 1; i <= altura; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
