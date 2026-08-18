beb2438e2c43fa9275b398c7b57f0b0b98b1c256
JUAN PORRAS <juanporras1810@gmail.com>
2026-08-17 19:32:44 -0500

    Ejercicios de arreglos

--- CONSOLA/ARREGLOS/Practicas/ejercicio06/Program.cs
+++ CONSOLA/ARREGLOS/Practicas/ejercicio06/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿//Promedio de notas: Calcule el promedio de 5 notas almacenadas en un arreglo.
+
+Console.WriteLine("=== PROMEDIO DE NOTAS ===");
+
+//Arreglo
+double[] notas = { 4, 2, 10, 6, 8 };
+
+//Ciclo que suma notas
+double suma = 0;
+Console.Write($"Notas: [ ");
+for (int i = 0; i < notas.Length; i++)
+{
+    if (i < notas.Length - 1)
+    {
+        Console.Write(notas[i] + ", ");
+    }
+    else
+    {
+        Console.WriteLine(notas[i] + " ]");
+    }
+    suma += notas[i];
+}
+
+//Calculo promedio
+double promedio = suma / notas.Length;
+Console.WriteLine($"Promedio: {promedio}");
---
+++ CONSOLA/ARREGLOS/Practicas/ejercicio07/Program.cs
@@ -0,0 +1,27 @@
+﻿//Promedio de notas: Calcule el promedio de 5 notas almacenadas en un arreglo.
+
+Console.WriteLine("=== PROMEDIO DE NOTAS ===");
+
+//Arreglo
+double[] notas = { 4, 2, 10, 6, 8 };
+
+//Ciclo que suma notas
+double suma = 0;
+Console.Write($"Arreglo original: [ ");
+for (int i = 0; i < notas.Length; i++)
+{
+    if (i < notas.Length - 1)
+    {
+        Console.Write(notas[i] + ", ");
+    }
+    else
+    {
+        Console.WriteLine(notas[i] + " ]");
+    }
+    suma += notas[i];
+}
+
+//Calculo promedio
+
+double promedio = suma / notas.Length;
+Console.WriteLine($"Promedio: {promedio}");
