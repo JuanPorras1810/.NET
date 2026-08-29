f579b161b40a5e04bf83d501282995f263a57e7c
JUAN PORRAS <juanporras1810@gmail.com>
2026-08-28 20:12:40 -0500

    Ejercicio con entity framework

---
+++ ESCRITORIO/ENTITY FRAMEWORK/NaturVida.App/Data/NaturVidaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NaturVida.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaturVida.App.Data
+{
+    internal class NaturVidaContext:DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=NaturVidaDB;Trusted_Connection=true;");
+        }
+    }
+}
